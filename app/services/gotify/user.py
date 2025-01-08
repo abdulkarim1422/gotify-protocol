30a87ad8a8b1e16e54caeb686af3b1081575dc27
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-09 00:50:58 +0300

    Add functions to retrieve current user and all users from Gotify

--- app/services/gotify/user.py
+++ app/services/gotify/user.py
@@ -1,5 +1,16 @@
 import requests
+from requests.auth import HTTPBasicAuth
 from app.initializers import env
+
+def current_user(username, password):
+    url = f"{env.GOTIFY_URL}/current/user"
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+    return response.json() # {'id': 1, 'name': 'xxxxxxx', 'admin': True}
+
+def get_all_users():
+    url = f"{env.GOTIFY_URL}/user?token={env.GOTIFY_ADMIN_TOKEN}"
+    response = requests.get(url)
+    return response.json() # [{'id': 1, 'name': 'xxxxxxx', 'admin': True}, {'id': 2, 'name': 'xxxxxxx', 'admin': False}]
 
 def create_user(username, password):
     url = f"{env.GOTIFY_URL}/user?token={env.GOTIFY_ADMIN_TOKEN}"
@@ -12,24 +23,17 @@ def create_user(username, password):
     response = requests.post(url, json=user_data)
     return response.json() # {'id': 2, 'name': 'xxxxxxx', 'admin': False}
 
-def get_all_users():
-    url = f"{env.GOTIFY_URL}/user?token={env.GOTIFY_ADMIN_TOKEN}"
-    response = requests.get(url)
-    return response.json() # [{'id': 1, 'name': 'xxxxxxx', 'admin': True}, {'id': 2, 'name': 'xxxxxxx', 'admin': False}]
-
 def get_user_by_id(user_id):
     url = f"{env.GOTIFY_URL}/user/{user_id}?token={env.GOTIFY_ADMIN_TOKEN}"
     response = requests.get(url)
     return response.json() # {'id': 2, 'name': 'xxxxxxx', 'admin': False}
     
 def get_user_by_username(username):
-    url = f"{env.GOTIFY_URL}/user?token={env.GOTIFY_ADMIN_TOKEN}"
-    response = requests.get(url)
-    users = response.json()
+    users = get_all_users()
     for user in users:
         if user['name'] == username:
-            return user
-    return None # {'id': 2, 'name': 'xxxxxxx', 'admin': False}
+            return user # {'id': 2, 'name': 'xxxxxxx', 'admin': False}
+    return None 
 
 def update_user(user_id, username, password):
     url = f"{env.GOTIFY_URL}/user/{user_id}?token={env.GOTIFY_ADMIN_TOKEN}"
