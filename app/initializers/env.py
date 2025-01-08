f5182a74b852cdd04be8ed86468dc3ac0af9e6b5
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-08 20:46:21 +0300

    Add environment variable loader for configuration settings

---
+++ app/initializers/env.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file
+load_dotenv()
+
+# Access environment variables
+
+# Gotify
+GOTIFY_DOMAIN = os.getenv('MAIN_DOMAIN')
+GOTIFY_ADMIN_TOKEN = os.getenv('GOTIFY_ADMIN_TOKEN') # must be stable token
+
+# DB
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
