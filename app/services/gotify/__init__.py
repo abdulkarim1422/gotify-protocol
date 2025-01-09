131287a7ec399d9dcbd1794acb7810dc46a3283a
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-09 23:44:20 +0300

    Refactor import statements in gotify service for consistency

--- app/services/gotify/__init__.py
+++ app/services/gotify/__init__.py
@@ -1,4 +1,4 @@
-import application
-import client
-import message
-import user
+from app.services.gotify import application
+from app.services.gotify import client
+from app.services.gotify import message
+from app.services.gotify import user
