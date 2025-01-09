360cadecdf932b692424745b48f7434526758a2e
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-10 00:27:38 +0300

    Set default project name in notification service and ensure successful message sending

--- app/services/notification_service.py
+++ app/services/notification_service.py
@@ -3,8 +3,10 @@ from app.services import gotify
 from app.services import application_service
 
 def send_notification(username, projectname, title, content):
+    if not projectname:
+        projectname = "default"
     app = application_service.check_app(username=username, app_name=projectname)
     gotify.message.create_message(app_token=app.app_token, title=title, message_body=content)
-
+    return True
 
 
