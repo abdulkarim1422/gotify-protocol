e66adfcbc53b173657d080123f7be037cb20cbe8
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-10 00:54:12 +0300

    Update user creation script to accept port input and simplify URL construction

--- setup/create_users.py
+++ setup/create_users.py
@@ -1,23 +1,22 @@
 import csv
 import requests
-import os
 
 # Read the CSV file
 with open('contacts.csv', mode='r') as file:
     csv_reader = csv.reader(file)
     rows = list(csv_reader)
 
-url = os.getenv('GOTIFY_URL') + '/user'
+port = input("port: ")
 
 for row in rows:
     username = row[0]
     password = row[1]
 
     # Define the URL and payload
-    payload = {'username': username, 'password': password}
+    url = "http://127.0.0.1:" + port + '/user' + "?username=" + username + "&password=" + password
 
     # Make the POST request
-    response = requests.post(url, data=payload)
+    response = requests.post(url)
 
     # Print the response
     print(response.text)
