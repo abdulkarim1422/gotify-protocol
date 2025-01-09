a84351641bdb4b0f25b7718e7d5fe83d2703fb82
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-01-09 21:54:19 +0300

    Refactor User and App models by commenting out unused Project class and project_id field

--- app/models.py
+++ app/models.py
@@ -12,20 +12,20 @@ class User(SQLModel, table=True):
     updated_at: datetime = Field(default=datetime.now())
     client_token: str
 
-# database class to store user projects (team side)
-class Project(SQLModel, table=True):
-    id: Optional[uuid.UUID] = Field(default=uuid.uuid4(), primary_key=True)
-    name: str
-    user_id: uuid.UUID
-    created_at: datetime = Field(default=datetime.now())
-    updated_at: datetime = Field(default=datetime.now())
+# # database class to store user projects (team side)
+# class Project(SQLModel, table=True):
+#     id: Optional[uuid.UUID] = Field(default=uuid.uuid4(), primary_key=True)
+#     name: str
+#     user_id: uuid.UUID
+#     created_at: datetime = Field(default=datetime.now())
+#     updated_at: datetime = Field(default=datetime.now())
 
 # database class to store user apps (gotify side)
 class App(SQLModel, table=True):
     id: Optional[uuid.UUID] = Field(default=uuid.uuid4(), primary_key=True)
     name: str
     user_id: uuid.UUID
-    project_id: str
+    # project_id: str
     created_at: datetime = Field(default=datetime.now())
     updated_at: datetime = Field(default=datetime.now())
     app_token: str
